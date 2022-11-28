d47f6edd7371d8eae1fed79748b217c07ce141c4
Dizy <dev@dizy.cc>
2022-11-28 11:55:06 -0500

    Change storing structure

--- bfs_data_generator/src/main.ts
+++ bfs_data_generator/src/main.ts
@@ -9,13 +9,14 @@ const MAX_DEGREE = Number(process.env.MAX_DEGREE || GRAPH_SIZE);
 function generateGraph(): number[][] {
   const graph: number[][] = [];
   for (let i = 0; i < GRAPH_SIZE; i++) {
-    graph.push(new Array(GRAPH_SIZE).fill(0));
+    graph.push([]);
     for (let j = 0; j < Math.floor(Math.random() * MAX_DEGREE); j++) {
       const neighbor = Math.floor(Math.random() * GRAPH_SIZE);
-      if (neighbor !== i) {
-        graph[i][neighbor] = 1;
+      if (neighbor !== i && !graph[i].includes(neighbor)) {
+        graph[i].push(neighbor);
       }
     }
+    graph[i].sort((a, b) => a - b);
   }
   return graph;
 }
@@ -24,10 +25,13 @@ export async function main(): Promise<void> {
   await FS.ensureDir(OUT_DIR);
   const outPath = Path.join(OUT_DIR, `graph_d${GRAPH_SIZE}_s${SAMPLES}.txt`);
   console.log(`Writing to ${outPath}, generating ${SAMPLES} graphs...`);
-  await FS.writeFile(outPath, `${SAMPLES}`);
+  await FS.writeFile(outPath, `${SAMPLES}\n`);
   for (let i = 0; i < SAMPLES; i++) {
-    const graph = generateGraph().flat();
-    const data = "\n" + graph.join(" ");
+    const graph = generateGraph();
+    let data = `${graph.length}\n`;
+    for (const node of graph) {
+      data += `${node.length} ${node.join(" ")}\n`;
+    }
     await FS.appendFile(outPath, data);
     console.log(`Generated sample ${i + 1} of ${SAMPLES}.`);
   }
